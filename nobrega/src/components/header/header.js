aa6a0d899ea83f0a7b9df5f7275fee8a890f8fce
Ariel Freitas <94645030+ArielFSantos@users.noreply.github.com>
2024-06-20 18:01:36 -0300

    att

--- nobrega/src/components/header/header.js
+++ nobrega/src/components/header/header.js
@@ -2,16 +2,20 @@ import './header.css'
 
 function Header(){
     return(
-        <header>
-            <h1>Fernanda Nobrega</h1>
-            <nav>
-                <div>Inicio</div>
-                <div>Sobre Mim</div>
-                <div>Fotos</div>
-                <div>Contato</div>
-            </nav>
-            
-        </header>
+        <body>
+            <header>
+                <h1>Fernanda Nobrega</h1>
+                <nav>
+                    <div>Inicio</div>
+                    <div>Sobre Mim</div>
+                    <div>Fotos</div>
+                    <div>Contato</div>
+                </nav>
+            </header>
+            <footer>
+                <p>Desenvolvido por Ariel Freitas</p>
+            </footer>
+        </body>
     )
 }
 export default Header
