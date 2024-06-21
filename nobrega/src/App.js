f48b0fd1b75856146c9c2c834d1abfa755264f2e
Ariel Freitas <94645030+ArielFSantos@users.noreply.github.com>
2024-06-21 12:43:07 -0300

    att

--- nobrega/src/App.js
+++ nobrega/src/App.js
@@ -1,14 +1,9 @@
 import Header from "./components/header/header";
-import Footer from "./components/footer/footer";
-
 function App() {
 
 
   return (
     <Header/>
-  );
-  return (
-    <Footer/>
   );
 }
 
