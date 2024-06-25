87441b6113b055ba0d0328fd89e61804ae1bde99
Ariel Freitas <94645030+ArielFSantos@users.noreply.github.com>
2024-06-25 15:32:07 -0300

    att

--- nobrega/src/components/home/home.js
+++ nobrega/src/components/home/home.js
@@ -1,13 +1,9 @@
 import './home.css'
 
 function Home(){
-    function teste(){
-        window.alert('Funcionou')
-    }
     return(
         <>
         <div className='bg-1'></div>
-        <button onClick={teste}>Ativar</button>
         <div className='bg-2'></div>
         <div className='bg-3'></div>
         <div className='bg-4'></div>
