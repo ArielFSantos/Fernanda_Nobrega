6528a87f62620e30519e1e3e56803bdea0170520
Ariel Freitas <94645030+ArielFSantos@users.noreply.github.com>
2024-06-25 15:01:48 -0300

    testando Eventos

--- nobrega/src/components/main/main.js
+++ nobrega/src/components/main/main.js
@@ -1,9 +1,15 @@
 import './main.css'
 
 function main(){
+    function teste(){
+        window.alert('Funcionou')
+        console.log('Funcionou')
+    }
     return(
         <>
-        <div className='bg-1'></div>
+        <div className='bg-1'>
+            <button onClick={teste}>Clique</button>
+        </div>
         <div className='bg-2'></div>
         <div className='bg-3'></div>
         <div className='bg-4'></div>
