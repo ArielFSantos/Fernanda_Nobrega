352a5a8334230a7cdf781ee5734bec67ae59a92d
Ariel Freitas <94645030+ArielFSantos@users.noreply.github.com>
2024-06-26 18:53:41 -0300

    insert
    inserido texto de boas vindas

--- nobrega/src/components/home/home.js
+++ nobrega/src/components/home/home.js
@@ -8,9 +8,24 @@ function Home(){
             architecto aliquam veniam delectus quasi ex ut deserunt tenetur necessitatibus repellendus.
             </p>
         </div>
-        <div className='bg-2'></div>
-        <div className='bg-3'></div>
-        <div className='bg-4'></div>
+        <div className='bg-2'>
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+            Quibusdam ratione mollitia est esse eaque voluptate voluptatem rerum consequuntur quod, 
+            architecto aliquam veniam delectus quasi ex ut deserunt tenetur necessitatibus repellendus.
+        </p>
+        </div>
+        <div className='bg-3'>
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+            Quibusdam ratione mollitia est esse eaque voluptate voluptatem rerum consequuntur quod, 
+            architecto aliquam veniam delectus quasi ex ut deserunt tenetur necessitatibus repellendus.
+        </p>
+        </div>
+        <div className='bg-4'>
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+            Quibusdam ratione mollitia est esse eaque voluptate voluptatem rerum consequuntur quod, 
+            architecto aliquam veniam delectus quasi ex ut deserunt tenetur necessitatibus repellendus.
+        </p>
+        </div>
         </>
     )
 }
