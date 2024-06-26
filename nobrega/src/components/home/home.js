69ac7ca64c27dfd9b7de2941f760b607eab4c73a
Ariel Freitas <94645030+ArielFSantos@users.noreply.github.com>
2024-06-26 18:24:15 -0300

    att

--- nobrega/src/components/home/home.js
+++ nobrega/src/components/home/home.js
@@ -2,7 +2,12 @@ import './home.css'
 function Home(){
     return(
         <>
-        <div className='bg-1'></div>
+        <div className='bg-1'> 
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+            Quibusdam ratione mollitia est esse eaque voluptate voluptatem rerum consequuntur quod, 
+            architecto aliquam veniam delectus quasi ex ut deserunt tenetur necessitatibus repellendus.
+            </p>
+        </div>
         <div className='bg-2'></div>
         <div className='bg-3'></div>
         <div className='bg-4'></div>
