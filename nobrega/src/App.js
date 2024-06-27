dd1f10c130ebb87fd5ce1aba262977e0fd9fd00f
Ariel Freitas <94645030+ArielFSantos@users.noreply.github.com>
2024-06-27 18:18:12 -0300

    reestruct

--- nobrega/src/App.js
+++ nobrega/src/App.js
@@ -1,13 +1,7 @@
 import Header from "./components/header/header";
 import Footer from "./components/footer/footer";
 import Home from "./components/Pages/home/home";
-import About from "./components/Pages/about/about";
-import Contact from "./components/Pages/contact/contact";
-import Photos from "./components/Pages/photos/photos";
 
-
-
-import {BrowserRouter as Router, Switch, Route, Link} from 'react-router-dom'
 
 
 function App() {
@@ -15,40 +9,6 @@ function App() {
   return (
     <>
     <Header/>
-    <Router>
-      <ul>
-        <li>
-          <Link to="/">Home</Link>
-        </li>
-        <li>
-          <Link to="about">About Me</Link>
-        </li>
-        <li>
-          <Link to="photos">Photos</Link>
-        </li>
-        <li>
-          <Link to="contact">Contact</Link>
-        </li>
-      </ul>
-      <Switch>
-        <Route path="/">
-          <Home/>
-        </Route>
-        <Route path="/about">
-          <About/>
-        </Route>
-        <Route path="/photos">
-          <Photos/>
-        </Route>
-        <Route path="/contact">
-          <Contact/>
-        </Route>
-
-      </Switch>
-    </Router>
-
-
-
     <Home/>
     <Footer/>
 
--- nobrega/src/components/header/header.js
+++ nobrega/src/components/header/header.js
@@ -1,18 +1,46 @@
 import './header.css'
+import About from '../Pages/about/about';
+import Contact from '../Pages/contact/contact';
+import Photos from '../Pages/photos/photos';
+import Home from '../Pages/home/home';
+import {BrowserRouter as Router, Route, Link, Routes} from 'react-router-dom'
 
 function Header(){
     return(
         <>
             <header>
                 <h1>Fernanda Nobrega</h1>
-                <nav>
-                    <ul>
-                        <li>Inicio</li>
-                        <li>Sobre Mim</li>
-                        <li>Fotos</li>
-                        <li>Contato</li>
-                    </ul>
-                </nav>
+                <Router>
+            <ul>
+                <li>
+                <Link to="/">Home</Link>
+                </li>
+                <li>
+                <Link to="about">About Me</Link>
+                </li>
+                <li>
+                <Link to="photos">Photos</Link>
+                </li>
+                <li>
+                <Link to="contact">Contact</Link>
+                </li>
+            </ul>
+            <Routes>
+                <Route exact path="/">
+                    <Home/>
+                </Route>
+                <Route path="/about">
+                    <About/>
+                </Route>
+                <Route path="/photos">
+                    <Photos/>
+                </Route>
+                <Route path="/contact">
+                    <Contact/>
+                </Route>
+            </Routes>
+            </Router>
+
             </header>
         </>
     )
