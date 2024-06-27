d116f0210a09eabd08f93f7b632282e0e320e7ec
Ariel Freitas <94645030+ArielFSantos@users.noreply.github.com>
2024-06-27 18:02:40 -0300

    Pages
    estruturando Routter para alterar entre paginas

--- nobrega/src/App.js
+++ nobrega/src/App.js
@@ -1,13 +1,36 @@
 import Header from "./components/header/header";
 import Footer from "./components/footer/footer";
 import Home from "./components/home/home";
+import {BrowserRouter as Router, Switch, Route, Link} from 'react-router-dom'
+
+
 function App() {
 
   return (
     <>
     <Header/>
+    <Router>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="about">About Me</Link>
+        </li>
+        <li>
+          <Link to="photos">Photos</Link>
+        </li>
+        <li>
+          <Link to="contact">Contact</Link>
+        </li>
+      </ul>
+    </Router>
+
+
+
     <Home/>
     <Footer/>
+
     </>
   );
 }
