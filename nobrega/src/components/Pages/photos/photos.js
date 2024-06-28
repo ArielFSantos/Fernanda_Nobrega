14d479e6fe74479d911f42f4b96e294595fde607
Ariel Freitas <94645030+ArielFSantos@users.noreply.github.com>
2024-06-28 15:08:21 -0300

    iniciado tela das fotos

--- nobrega/src/components/Pages/photos/photos.js
+++ nobrega/src/components/Pages/photos/photos.js
@@ -1,7 +1,25 @@
+import styles from './photos.module.css'
+
 function Photos(){
     return(
         <>
-        <h1>Photos</h1>
+        <div className={styles.container} >
+            <div className={styles.fotos}>
+                <h1>Evento 1</h1>
+                <img src="" alt="" />
+            </div>
+            <div className={styles.fotos}>Imagem 2</div>
+            <div className={styles.fotos}>Imagem 3</div>
+            <div className={styles.fotos}>Imagem 4</div>
+            <div className={styles.fotos}>Imagem 5</div>
+            <div className={styles.fotos}>Imagem 6</div>
+            <div className={styles.fotos}>Imagem 7</div>
+            <div className={styles.fotos}>Imagem 8</div>
+            <div className={styles.fotos}>Imagem 9</div>
+            <div className={styles.fotos}>Imagem 10</div>
+
+            
+        </div>
         </>
     )
 }
