da0fadc1f8a297d9dd3485a6c76bd1a9c27ba969
Ariel Freitas <94645030+ArielFSantos@users.noreply.github.com>
2024-07-01 17:56:57 -0300

    Form
    configurando formularios

--- nobrega/src/components/Pages/contact/contact.js
+++ nobrega/src/components/Pages/contact/contact.js
@@ -5,7 +5,11 @@ function Contact(){
         <>   
         <h1 className={styles.title}>Orçamento</h1>
         <span className={styles.container} >
-            <p className={styles.text}>Para realizar um orçamento entre em contato através de minhas redes sociais ou preencha o formulario</p>
+            <aside className={styles.socialmidias}>
+                <p className={styles.text}>Para realizar um orçamento entre em contato através de minhas redes sociais ou preencha o formulario</p>
+                <div>Instagram</div>
+                <div>Whatsapp</div>
+            </aside>
             <form action="send" className={styles.formulario}>
                 <h2>Nome para contato:</h2>
                 <input type="text" placeholder='Nome'/>
@@ -17,6 +21,7 @@ function Contact(){
                 <input type="number" placeholder='Horas'/>
                 <h2>Descreva o Evento:</h2>
                 <input className={styles.description} type="text" placeholder='Descrição' />
+                <button className={styles.button} type='submit'>Enviar</button>
             </form>
         </span>
         </>
