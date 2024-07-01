3a5cd54ed4c78bba1d338bffadbec9dec2f011b5
Ariel Freitas <94645030+ArielFSantos@users.noreply.github.com>
2024-07-01 20:17:38 -0300

    Finish About Me
    Pagina "Sobre mim" completa ate o momento

--- nobrega/src/components/Pages/about/about.js
+++ nobrega/src/components/Pages/about/about.js
@@ -8,7 +8,7 @@ function About (){
             <h1 className={styles.title}>Sobre mim:</h1>
             <p className={styles.text}>
             Olá! Eu sou a Fernanda Nóbrega, uma fotógrafa apaixonada por capturar momentos autênticos e emoções verdadeiras. Desde criança, a fotografia é minha grande paixão e,
-            nos últimos 2 anos, tenho transformado essa paixão em uma profissão, dedicando-me a transformar sentimentos em imagens.
+            nos últimos 2 anos, tenho transformado essa paixão em uma profissão, dedicando-me a transformar sentimentos em imagens, criando memórias que você pode reviver para sempre.
             Minha missão é fazer com que cada foto conte uma história e transmita a essência de quem você é. Seja um sorriso sincero, um olhar contemplativo ou um gesto de carinho, estou aqui
             para registrar esses instantes preciosos com toda a sensibilidade que eles merecem.
             Vamos juntos eternizar os seus momentos mais especiais?
