c5c30cc097c852f73b38565d895c12db92ab7ea1
Ariel Freitas <94645030+ArielFSantos@users.noreply.github.com>
2024-07-06 23:35:09 -0300

    init image
    iniciado codigo masonry para galeria de imagens ( portifolio )

--- nobrega/src/components/Pages/photos/photos.js
+++ nobrega/src/components/Pages/photos/photos.js
@@ -1,26 +1,41 @@
-import styles from './photos.module.css'
+// src/components/Pages/photos/Photos.js
+import React from 'react';
+import Masonry from 'react-masonry-css';
+import styles from './photos.module.css';
 
-function Photos(){
-    return(
-        <>
-        <div className={styles.container} >
-            <div className={styles.fotos}>
-                <h1>Evento 1</h1>
-                <img src="" alt="" />
-            </div>
-            <div className={styles.fotos}>Imagem 2</div>
-            <div className={styles.fotos}>Imagem 3</div>
-            <div className={styles.fotos}>Imagem 4</div>
-            <div className={styles.fotos}>Imagem 5</div>
-            <div className={styles.fotos}>Imagem 6</div>
-            <div className={styles.fotos}>Imagem 7</div>
-            <div className={styles.fotos}>Imagem 8</div>
-            <div className={styles.fotos}>Imagem 9</div>
-            <div className={styles.fotos}>Imagem 10</div>
+const images = [
+    'https://picsum.photos/200/105',
+    'https://picsum.photos/140/200',
+    'https://picsum.photos/150/200',
+    'https://picsum.photos/100/200',
+    'https://picsum.photos/200/100',
+    'https://picsum.photos/100/215',
+    'https://picsum.photos/200/120',
+    'https://picsum.photos/200/110',
+];
 
-            
+function Photos() {
+    const breakpointColumnsObj = {
+        default: 4, // 3 colunas para telas padrão
+        1100: 2,    // 2 colunas para telas maiores que 1100px
+        700: 1      // 1 coluna para telas menores que 700px
+    };
+
+    return (
+        <div className={styles.container}>
+            <Masonry
+                breakpointCols={breakpointColumnsObj}
+                className={styles.myMasonryGrid}
+                columnClassName={styles.myMasonryGridColumn}
+            >
+                {images.map((src, index) => (
+                    <div key={index} className={styles.fotos}>
+                        <img src={src} alt={`Gallery Image ${index + 1}`} />
+                    </div>
+                ))}
+            </Masonry>
         </div>
-        </>
-    )
+    );
 }
-export default Photos
+
+export default Photos;
