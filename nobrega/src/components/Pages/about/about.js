319aa5b3dde0deebec1b2b1f548c4f921b18cd5d
Ariel Freitas <94645030+ArielFSantos@users.noreply.github.com>
2024-07-12 15:07:30 -0300

    style about
    melhoria do  design da pagina about

--- nobrega/src/components/Pages/about/about.js
+++ nobrega/src/components/Pages/about/about.js
@@ -1,22 +1,21 @@
-import styles from './about.module.css'
+import React from 'react'; 
+import styles from './about.module.css';
 
-function About (){
-    return(
-        <>
-        <div className={styles.container} >
-            <div className={styles.about}/>
-            <main>
-            <h1 className={styles.title}>Olá! Eu sou a Fernanda Nóbrega</h1>
-                <p className={styles.text}>
-                Uma fotógrafa goiana apaixonada por capturar momentos autênticos e emoções verdadeiras. Desde criança, a fotografia é minha grande paixão e,
-                nos últimos 2 anos, tenho transformado essa paixão em uma profissão, dedicando-me a transformar sentimentos em imagens, criando memórias que você pode reviver para sempre.
-                Minha missão é fazer com que cada foto conte uma história e transmita a essência de quem você é. Seja um sorriso sincero, um olhar contemplativo ou um gesto de carinho, estou aqui
-                para registrar esses instantes preciosos com toda a sensibilidade que eles merecem.
-                Vamos juntos eternizar os seus momentos mais especiais?
-                </p>
-            </main>
-        </div>
-        </>
-    )
-}
-export default About
+
+const About = () => (
+  <div className={styles.container}>
+    <div className={styles.about} />
+    <main>
+      <h1 className={styles.title}>Olá! Eu sou a Fernanda Nóbrega</h1>
+      <p className={styles.text}>
+        Uma fotógrafa goiana apaixonada por capturar momentos autênticos e emoções verdadeiras. Desde criança, a fotografia é minha grande paixão e,
+        nos últimos 2 anos, tenho transformado essa paixão em uma profissão, dedicando-me a transformar sentimentos em imagens, criando memórias que você pode reviver para sempre.
+        Minha missão é fazer com que cada foto conte uma história e transmita a essência de quem você é. Seja um sorriso sincero, um olhar contemplativo ou um gesto de carinho, estou aqui
+        para registrar esses instantes preciosos com toda a sensibilidade que eles merecem.
+        Vamos juntos eternizar os seus momentos mais especiais?
+      </p>
+    </main>
+  </div>
+);
+
+export default About;
