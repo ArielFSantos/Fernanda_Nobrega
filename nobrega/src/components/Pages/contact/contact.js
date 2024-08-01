5c9068d23144e90565bffc852d88de341965c9f4
Ariel Freitas <94645030+ArielFSantos@users.noreply.github.com>
2024-08-01 11:33:18 -0300

    external links
    melhoria do link externo

--- nobrega/src/components/Pages/contact/contact.js
+++ nobrega/src/components/Pages/contact/contact.js
@@ -6,11 +6,11 @@ import insta from '../../../image/insta.png';
 import whats from '../../../image/whatsapp.png';
 
 const ContactSchema = Yup.object().shape({
-    nome: Yup.string().required('Nome é obrigatório'),
-    telefone: Yup.string().required('Número de telefone é obrigatório'),
-    local: Yup.string().required('Local do evento é obrigatório'),
-    duracao: Yup.number().required('Duração do evento é obrigatória').positive().integer(),
-    descricao: Yup.string().required('Descrição é obrigatória')
+    nome: Yup.string().required('Nome obrigatório'),
+    telefone: Yup.string().required('Número de telefone obrigatório'),
+    local: Yup.string().required('Local do evento obrigatório'),
+    duracao: Yup.number().required('Duração do evento obrigatória').positive().integer(),
+    descricao: Yup.string().required('Descrição obrigatória')
 });
 
 function Contact() {
@@ -22,19 +22,15 @@ function Contact() {
                     <p className={styles.text}>
                         Para realizar um orçamento entre em contato através de minhas redes sociais ou preencha o formulário
                     </p>
-                    <div className={styles.externalLinks}>
-                        <div className={styles.containerInsta}>
-                            <a href="https://www.instagram.com/nobrega.photograph/" target="_blank" rel="noreferrer">
+                    <div className={styles.externalLinks}>     
+                            <a href="https://www.instagram.com/nobrega.photograph/" className={styles.containerInsta} target="_blank" rel="noreferrer">
                                 <img alt='contato Instagram' src={insta} className={styles.insta}></img>
+                                <h2>@nobrega.photograph</h2>
                             </a>
-                            <h2>@nobrega.photograph</h2>
-                        </div>
-                        <div className={styles.containerWhats}>
-                            <a href="https://wa.me/556292515025" target="_blank" rel="noreferrer">
+                            <a href="https://wa.me/556292515025" className={styles.containerWhats} target="_blank" rel="noreferrer">
                                 <img alt='contato whatsapp' src={whats} className={styles.whats}></img>
-                            </a>
-                            <h2>+55 62 99251-5025</h2>
-                        </div>
+                                <h2>+55 62 99251-5025</h2>
+                            </a>  
                     </div>
                 </aside>
                 <Formik
